93902a7cf94629f4e5bfe3bec2956150a0b70350
Gregor Obreza <gobreza@gmail.com>
2021-02-19 11:48:04 +0100

    secret removed

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mzp&lp5@21qsnkmqy8n1!ie%k&k14m9nhuq@ck(mlk+xg^rqz&'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
